33f0a73a2752ee6f9b1a6ad0eb4ae8029b8417c9
Aaron Kearns <akearns@usgs.gov>
2018-10-25 18:54:29 -0600

    Fix issue where data was not properly loaded into GUI frames

---
+++ src/test/java/com/isti/xmax/data/XMAXDataModuleTest.java
@@ -0,0 +1,29 @@
+package com.isti.xmax.data;
+
+import static org.junit.Assert.*;
+
+import com.isti.traceview.TraceView;
+import com.isti.traceview.TraceViewException;
+import com.isti.traceview.data.SourceFile;
+import com.isti.xmax.XMAXconfiguration;
+import java.io.File;
+import java.util.List;
+import org.junit.Test;
+
+public class XMAXDataModuleTest {
+
+
+  @Test
+  public void loadData() throws TraceViewException {
+    // NOTE: currently not matching anything as resources here are not included anywhere
+    String mask = "src/test/resources/*.seed";
+    XMAXconfiguration config = XMAXconfiguration.getInstance();
+    TraceView.setConfiguration(config);
+    TraceView.getConfiguration().setDataPath(mask);
+    XMAXDataModule xdm = XMAXDataModule.getInstance();
+    List<File> fileList = SourceFile.getDataFiles(mask);
+    File[] files = xdm.getDataFiles();
+    assertTrue(files.length > 0);
+  }
+
+}
