dc620fdde5b9e70208c4a8d17283e079bc808698
Kearns, Aaron (Contractor) M <akearns@contractor.usgs.gov>
2019-07-26 18:18:07 -0600

    Fix bug where filtered gapped data was offset incorrectly

--- src/main/java/com/isti/traceview/filters/FilterLP.java
+++ src/main/java/com/isti/traceview/filters/FilterLP.java
@@ -1,8 +1,6 @@
 package com.isti.traceview.filters;
 
 import asl.utils.FilterUtils;
-import asl.utils.TimeSeriesUtils;
-import com.isti.traceview.data.RawDataProvider;
 import com.isti.traceview.processing.LPFilterException;
 import org.apache.log4j.Logger;
 
@@ -10,23 +8,15 @@ import org.apache.log4j.Logger;
  * Low-pass Butterworth filter Algorithm is from Stearns, 1975
  */
 
-public class FilterLP implements IFilter {
+public class FilterLP extends AbstractFilter {
 	public static final String DESCRIPTION = "Apply Low Pass filter for selected channels";
 	public static final String NAME = "LP";
 
 	/** The Constant logger. */
 	private static final Logger logger = Logger.getLogger(FilterLP.class); // @jve:decl-index=0:
 
-	/**
-	 * number of filter sections
-	 */
-	int order = 0;
-	double cutFrequency = Double.NaN;
-	double sampleRate = 0.;
-
-	public int getMaxDataLength() {
-		return Integer.MAX_VALUE;
-	}
+	private double cutFrequency;
+	private int order;
 
 	/**
 	 * @param order
@@ -51,30 +41,11 @@ public class FilterLP implements IFilter {
 		return FilterLP.NAME;
 	}
 
-	/**
-	 * design routine
-	 * 
-	 * @param channel
-	 *            trace to retrieve information
-	 */
-	synchronized public void init(RawDataProvider channel) {
-		sampleRate = 1000.0 / channel.getSampleRate();
-	}
-
-	/**
-	 * Performs low-pass Butterworth filtering of a time series.
-	 * 
-	 * @param data
-	 *            = data array
-	 * @param length
-	 *            = number of samples in data array
-	 * @return filtered data array
-	 */
-	synchronized public double[] filter(double[] data, int length) throws LPFilterException {
+	@Override
+	double[] filterBackend(double[] data, int length) throws LPFilterException {
 		if (data.length > length)
 			throw new LPFilterException("Requested filtering length exceeds provided array length");
 
-		TimeSeriesUtils.demeanInPlace(data);
 		return FilterUtils.lowPassFilter(data, sampleRate, cutFrequency, order);
 	}
 
